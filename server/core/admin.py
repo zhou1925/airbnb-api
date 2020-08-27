be74643dbbed1ef8e948c1921263a55f1463a4e4
Zait <ayalaorez@gmail.com>
2020-08-27 16:41:00 -0500

    admin test added

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {
+            "fields": (
+                ('email', 'password')
+            ),
+        }),
+        (
+            ('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (
+            ('Important dates'), {'fields': ('last_login',)}
+        )
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+    
+
+admin.site.register(User, UserAdmin)
+
