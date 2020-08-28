981129b7b3ddc42fdcb3d84d0ea46ed70e5b4d34
Zait <ayalaorez@gmail.com>
2020-08-28 12:26:09 -0500

    register models in admin

--- server/rooms/admin.py
+++ server/rooms/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import HouseRule, Amenity, RoomType, Room, Photo, Service
 
-# Register your models here.
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(HouseRule)
+class HouseRuleAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(RoomType)
+class RoomTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline,]
+
+@admin.register(Service)
+class Service(admin.ModelAdmin):
+    pass
+
